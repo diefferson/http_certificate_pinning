87abd8ead250239ae4afae09cc475378c79d4751
Nikita Sirovsky <sirovsky@sbyte.dev>
2021-10-29 12:40:50 +0300

    Move secure_http_client to a separate folder

--- lib/http_certificate_pinning.dart
+++ lib/http_certificate_pinning.dart
@@ -1,5 +1,6 @@
 library http_certificate_pinning;
 
 export 'src/http_certificate_pinning.dart';
+export 'src/http_client/secure_http_client.dart';
 export 'src/exceptions/exceptions.dart';
 export 'src/dio/certificate_pinning_interceptor.dart';
