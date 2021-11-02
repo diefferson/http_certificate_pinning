9664539d8d45167eb4950fed1bc5f434aaacd54e
Justin Alexander <thelonecabbage@gmail.com>
2021-11-02 16:44:56 +0200

    mege

---
+++ lib/src/dio/certificate_pinning_interceptor.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+import 'package:http_certificate_pinning/http_certificate_pinning.dart';
+
+import '../exceptions/exceptions.dart';
+
+class CertificatePinningInterceptor extends Interceptor {
+  final List<String> _allowedSHAFingerprints;
+  final int _timeout;
+  final Set<String> verifiedURLs = {};
+  Future<String>? secure = Future.value('');
+
+  CertificatePinningInterceptor({
+    List<String>? allowedSHAFingerprints,
+    int timeout = 0,
+  })  : _allowedSHAFingerprints = allowedSHAFingerprints != null
+            ? allowedSHAFingerprints
+            : <String>[],
+        _timeout = timeout;
+
+  @override
+  Future onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    try {
+      // skip verification if already verified, performance
+      if (verifiedURLs.contains(options.baseUrl)) {
+        return super.onRequest(options, handler);
+      }
+      // iOS bug: Alamofire is failing to return parallel requests for certificate validation
+      if (Platform.isIOS && secure != null) {
+        await secure;
+      }
+
+      secure = HttpCertificatePinning.check(
+        serverURL: options.baseUrl,
+        headerHttp: options.headers.map((a, b) => MapEntry(a, b.toString())),
+        sha: SHA.SHA256,
+        allowedSHAFingerprints: _allowedSHAFingerprints,
+        timeout: _timeout,
+      );
+
+      secure?.whenComplete(() => secure = null);
+      final secureString = await secure ?? '';
+
+      if (secureString.contains('CONNECTION_SECURE')) {
+        verifiedURLs.add(options.baseUrl);
+        return super.onRequest(options, handler);
+      } else {
+        handler.reject(
+          DioError(
+            requestOptions: options,
+            error: CertificateNotVerifiedException(),
+          ),
+        );
+      }
+    } on Exception catch (e) {
+      dynamic error;
+
+      if (e is PlatformException && e.code == 'CONNECTION_NOT_SECURE') {
+        error = const CertificateNotVerifiedException();
+      } else {
+        error = CertificateCouldNotBeVerifiedException(e);
+      }
+
+      handler.reject(
+        DioError(
+          requestOptions: options,
+          error: error,
+        ),
+      );
+    }
+  }
+}
