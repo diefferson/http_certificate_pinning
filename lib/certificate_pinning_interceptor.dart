269171376e2c0ee857237619d2e0967ca80ac307
Justin Alexander <thelonecabbage@gmail.com>
2021-11-02 11:45:21 +0200

    fix ios paralell

--- lib/certificate_pinning_interceptor.dart
+++ lib/certificate_pinning_interceptor.dart
@@ -1,24 +1,39 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:http_certificate_pinning/http_certificate_pinning.dart';
 
 class CertificatePinningInterceptor extends Interceptor {
   final List<String> _allowedSHAFingerprints;
-
+  final Set<String> verifiedURLs = {};
+  Future<String>? secure = Future.value('');
   CertificatePinningInterceptor(this._allowedSHAFingerprints);
 
   @override
   Future onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    final secure = await HttpCertificatePinning.check(
+// skip verification if already verified, performance
+    if (verifiedURLs.contains(options.baseUrl)) {
+      return super.onRequest(options, handler);
+    }
+// iOS bug: Alamofire is failing to return parallel requests for certificate validation
+    if (Platform.isIOS && secure != null) {
+      await secure;
+    }
+
+    secure = HttpCertificatePinning.check(
         serverURL: options.baseUrl,
         headerHttp: options.headers.map((a, b) => MapEntry(a, b.toString())),
         sha: SHA.SHA256,
         allowedSHAFingerprints: _allowedSHAFingerprints,
         timeout: 50);
+    secure?.whenComplete(() => secure = null);
+    final secureString = await secure;
 
-    if (secure.contains("CONNECTION_SECURE")) {
+    if (secureString!.contains("CONNECTION_SECURE")) {
+      // record success
+      verifiedURLs.add(options.baseUrl);
       return super.onRequest(options, handler);
     } else {
       throw Exception("CONNECTION_NOT_SECURE");
