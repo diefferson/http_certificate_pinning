d72ae36fa5a38d719becff2ce96e3704ca6aaa0e
Gustavo Guerra <gustavoguerra.gr@gmail.com>
2025-02-18 12:50:17 -0300

    Fix call Alamofire

--- ios/Classes/SwiftHttpCertificatePinningPlugin.swift
+++ ios/Classes/SwiftHttpCertificatePinningPlugin.swift
@@ -5,10 +5,9 @@ import Alamofire
 
 public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
 
-    var session: Session!
     var fingerprints: [String]?
     var flutterResult: FlutterResult?
-
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "http_certificate_pinning", binaryMessenger: registrar.messenger())
         let instance = SwiftHttpCertificatePinningPlugin()
@@ -21,127 +20,69 @@ public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
             if let args = call.arguments as? [String: Any] {
                 self.check(call: call, args: args, flutterResult: result)
             } else {
-                result(
-                    FlutterError(
-                        code: "Invalid Arguments",
-                        message: "Please specify arguments",
-                        details: nil
-                    )
-                )
+                result(FlutterError(code: "Invalid Arguments", message: "Please specify arguments", details: nil))
             }
         default:
             result(FlutterMethodNotImplemented)
         }
     }
 
-    public func check(
-        call: FlutterMethodCall,
-        args: [String: Any],
-        flutterResult: @escaping FlutterResult
-    ) {
+    public func check(call: FlutterMethodCall, args: [String: Any], flutterResult: @escaping FlutterResult) {
         guard let urlString = args["url"] as? String,
               let headers = args["headers"] as? [String: String],
               let fingerprints = args["fingerprints"] as? [String],
-              let type = args["type"] as? String
-        else {
-            flutterResult(
-                FlutterError(
-                    code: "Params incorrect",
-                    message: "Les params sont incorrect",
-                    details: nil
-                )
-            )
+              let type = args["type"] as? String else {
+            flutterResult(FlutterError(code: "Params incorrect", message: "Invalid parameters", details: nil))
             return
         }
 
-        self.fingerprints = fingerprints
-
-        let timeout = args["timeout"] as? Int ?? 60
-
-        // Configuração da sessão com delegate personalizado
-        let configuration = URLSessionConfiguration.default
-        configuration.timeoutIntervalForRequest = TimeInterval(timeout)
-
-        session = Session(configuration: configuration)
-
-        var resultDispatched = false
-
+        self.fingerprints = fingerprints.map { $0.replacingOccurrences(of: " ", with: "") }
+        let timeout = (args["timeout"] as? Int) ?? 60
+        
+        // Configuração do ServerTrustManager para validar o certificado
+        let serverTrustManager = ServerTrustManager(evaluators: [
+            urlString: CustomServerTrustEvaluator(allowedFingerprints: self.fingerprints!, hashType: type)
+        ])
+        
+        let session = Session(configuration: .default, serverTrustManager: serverTrustManager)
+        
         session.request(urlString, method: .get, headers: HTTPHeaders(headers)).validate().responseJSON { response in
             switch response.result {
             case .success:
-                if !resultDispatched {
-                    flutterResult("CONNECTION_SECURE")
-                    resultDispatched = true
-                }
+                flutterResult("CONNECTION_SECURE")
             case .failure(let error):
-                if !resultDispatched {
-                    flutterResult(
-                        FlutterError(
-                            code: "URL Format",
-                            message: error.localizedDescription,
-                            details: nil
-                        )
-                    )
-                    resultDispatched = true
-                }
+                flutterResult(FlutterError(code: "CONNECTION_NOT_SECURE", message: error.localizedDescription, details: nil))
             }
         }
+    }
+}
 
-        // Configuração do delegate para lidar com desafios de autenticação
-        session.delegate.taskDidReceiveChallenge = { session, task, challenge in
-            guard let serverTrust = challenge.protectionSpace.serverTrust,
-                  let certificate = SecTrustGetCertificateAtIndex(serverTrust, 0) else {
-                flutterResult(
-                    FlutterError(
-                        code: "ERROR CERT",
-                        message: "Invalid Certificate",
-                        details: nil
-                    )
-                )
-                return (.cancelAuthenticationChallenge, nil)
-            }
+// Classe personalizada para validar o certificado com SHA-256 ou SHA-1
+class CustomServerTrustEvaluator: ServerTrustEvaluating {
+    let allowedFingerprints: [String]
+    let hashType: String
+    
+    init(allowedFingerprints: [String], hashType: String) {
+        self.allowedFingerprints = allowedFingerprints
+        self.hashType = hashType
+    }
 
-            // Define políticas SSL para verificação de nome de domínio
-            let policies = [SecPolicyCreateSSL(true, challenge.protectionSpace.host as CFString)]
-            SecTrustSetPolicies(serverTrust, policies as CFTypeRef)
+    func evaluate(_ trust: SecTrust, forHost host: String) throws {
+        guard let certificate = SecTrustGetCertificateAtIndex(trust, 0) else {
+            throw AFError.serverTrustEvaluationFailed(reason: .noCertificatesFound)
+        }
 
-            // Avalia o certificado do servidor
-            var result: SecTrustResultType = .invalid
-            SecTrustEvaluate(serverTrust, &result)
-            let isServerTrusted = (result == .unspecified || result == .proceed)
+        let serverCertData = SecCertificateCopyData(certificate) as Data
+        var serverCertSha = serverCertData.sha256().toHexString()
 
-            // Obtém o hash do certificado
-            let serverCertData = SecCertificateCopyData(certificate) as Data
-            var serverCertHash = serverCertData.sha256().toHexString()
+        if hashType == "SHA1" {
+            serverCertSha = serverCertData.sha1().toHexString()
+        }
 
-            if type == "SHA1" {
-                serverCertHash = serverCertData.sha1().toHexString()
-            }
+        let isSecure = allowedFingerprints.contains { $0.caseInsensitiveCompare(serverCertSha) == .orderedSame }
 
-            // Verifica se o hash do certificado está na lista de fingerprints
-            let isSecure = self.fingerprints?.contains { fingerprint in
-                fingerprint.replacingOccurrences(of: " ", with: "").caseInsensitiveCompare(serverCertHash) == .orderedSame
-            } ?? false
-
-            if isServerTrusted && isSecure {
-                if !resultDispatched {
-                    flutterResult("CONNECTION_SECURE")
-                    resultDispatched = true
-                }
-            } else {
-                if !resultDispatched {
-                    flutterResult(
-                        FlutterError(
-                            code: "CONNECTION_NOT_SECURE",
-                            message: nil,
-                            details: nil
-                        )
-                    )
-                    resultDispatched = true
-                }
-            }
-
-            return (.cancelAuthenticationChallenge, nil)
+        if !isSecure {
+            throw AFError.serverTrustEvaluationFailed(reason: .defaultEvaluationFailed(output: "Fingerprint mismatch"))
         }
     }
 }
